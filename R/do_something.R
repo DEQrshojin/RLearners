67aab7f88ec1c1c2ea9307990ec0deaca17bb1c2
shojiwan <40436662+shojiwan@users.noreply.github.com>
2018-11-29 10:21:35 -0800

    Ready for the meeting!

---
+++ R/do_something.R
@@ -0,0 +1,118 @@
+# General notes:
+# To execute an individual line in the script the keyboard stroke is Ctrl-Enter
+# To execute the source script the keyboard stroke is Ctrl-Shft-Enter
+
+# LIBRARIES????
+library(lubridate)
+library(reshape2)
+library(ggplot2)
+
+# 1) Data types -- instances of data types are called objects
+#   a) A basic object
+someWords <- 'some words'
+
+#   b) How to view the data
+someWords           # You can also type this into the console
+
+print(someWords)
+
+View(someWords)     # You can also type this into the console
+                    # --OR-- If it's an object with more complex dimensional properties =====>>
+                    #  you can click on it in the Environment pane to the right         =====>>
+
+#   c) Here are some more objects
+aNumber <- 12345
+
+aTrueOrFalseThing <- TRUE
+
+anOrderedVector <- 1 : 100
+
+aRandomVector <- runif(100, 0.0, 1.0)
+
+roundedRandomVector <- round(aRandomVector, digits = 2)
+
+someMatrix = as.matrix(cbind(roundedRandomVector, round(runif(100, 0.0, 1.0), 2)))
+
+matrix2DataFrame = data.frame(someMatrix)
+
+someList = list(someWords, aNumber, aTrueOrFalseThing, aRandomVector, roundedRandomVector, someMatrix, matrix2DataFrame)
+
+# 2) GO GIT SOME DATA
+#   c) You can also type this into the same line
+data <- read.csv('E:/R/RLearners/R/data/flow_data.csv')
+
+#   a) Specify the location of the data
+data <- 'E:/R/RLearners/R/data/flow_data.csv'
+
+#   b) Tell R to go retrieve the data
+data <- read.csv(data)
+
+# 3) Do some things to the data
+#   a) subsets of the data
+View(data$DATE)
+
+data[849, 3]
+
+View(data[1 : 10, ])
+
+#   b) coerce data into correct formats
+data$DATE = as.Date(data$DATE, format = "%m/%d/%Y")
+
+str(data)
+
+data$DATE = as.POSIXct(data$DATE, format = "%Y-%m-%d", tz = 'America/Los_Angeles')
+
+data$DATE = data$DATE + hours(7)
+
+data$DATE = ifelse(hour(data$DATE) == 23, data$DATE + hours(1), data$DATE)
+
+data$DATE = as.POSIXct(data$DATE, origin = '1970-01-01')
+
+# 4) Plot the data
+#   a) base
+plot(data)
+
+#   b) ggplot
+flowPlot1 <- ggplot(data, aes(x = DATE, y = Gage)) + geom_point()
+
+plot(flowPlot1)
+
+flowPlot1 <- ggplot(data) + geom_point(aes(x = DATE, y = Gage)) +
+             geom_line(aes(x = DATE, y = Model), color = 'Blue')
+
+plot(flowPlot1)
+
+flowPlot1 <- ggplot(data) + geom_point(aes(x = DATE, y = Gage)) +
+             geom_line(aes(x = DATE, y = Model), color = 'Blue') +
+             scale_y_log10(limits = c(10, 40000))
+
+plot(flowPlot1)
+
+sameData = melt(data, id.vars = "DATE")
+
+flowPlot2 <- ggplot(sameDate,
+                    aes(x = DATE, y = value, group = variable, color = variable)) +
+             geom_line() + scale_y_log10(limits = c(10, 40000))
+
+plot(flowPlot2)
+
+# 5) Analyze the data
+#   a) Rename the columns of the 'sameData' data frame
+names(sameData) = c('date', 'srce', 'q_cfs')
+
+#   a) Calculate annual runoff volumes, i.e, daily flow in cfs to daily volume in acre-feet
+sameData$v_af <- sameData$q_cfs * 86400 / 43560
+
+sameData$year <- year(sameData$date)
+
+annVol <- dcast(sameData, year ~ srce, value.var = 'v_af', fun.aggregate = sum) # cast ~ pivot table in excel
+
+meanAnnVol <- list('model' = mean(unlist(annVol[, 2])),
+                   'gage' = mean(unlist(annVol[, 3])))
+
+annVol = melt(annVol, id.vars = "year")
+
+annVolPlot <- ggplot(data = annVol, aes(x = year, y = value, group = variable, color = variable)) + geom_col()
+
+plot(annVolPlot)
+
---
+++ R/do_something_from_nothing.R
@@ -0,0 +1,39 @@
+# General notes:
+# To execute an individual line in the script the keyboard stroke is Ctrl-Enter
+# To execute the source script the keyboard stroke is Ctrl-Shft-Enter
+
+# LIBRARIES????
+
+# 1) Data types -- instances of data types are called objects
+
+#     a) A basic object
+
+#     b) How to view the data
+
+#     c) Here are some more objects
+
+# 2) Go get some data!
+
+#     a) Retrieve data from an external source
+
+#     b) Specify the location of the data
+
+#     c) Tell R to go retrieve the data
+
+# 3) Do some things to the data
+
+#     a) Subsetting data
+
+#     b) Coercing data
+
+# 4) Plot the data
+
+#     a) Base graphing
+
+#     b) ggplot
+
+# 5) Analyze the data
+
+#     a) Rename the columns
+
+#     a) Calculate annual runoff volumes
